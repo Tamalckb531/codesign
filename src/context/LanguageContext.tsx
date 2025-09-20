98f287b804f26f9db2da24919d90d1c87a444295
Tamal <ckbtamaldipnew@gmail.com>
2025-09-20 20:51:21 +0600

    add Language Context provider -> give the code provider context of syntax highlighter for different language

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+type LanguageContextType = {
+  language: string;
+  setLanguage: (language: string) => void;
+};
+
+export const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
+
+const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
+  const [language, setLanguage] = useState("javascript");
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+export default LanguageProvider;
