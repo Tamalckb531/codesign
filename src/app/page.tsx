0dc4fcf58903e3aa94fa29000a6642fa6d89d1e2
Tamal <ckbtamaldipnew@gmail.com>
2025-09-20 23:52:43 +0600

    add intial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
+import CodeEditor from "@/components/CodeEditor";
+import Dock from "@/components/Dock";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className=" text-5xl flex">
-      <Button>Mocks</Button>
-      <div className="flex flex-row items-center justify-center text-red-500">
-        {" "}
-        <p className=" text-red-500">ji</p>
-        <p>sadf</p>
-        <p>fsd</p>
-      </div>
+    <main className=" max-h-screen flex flex-col items-center justify-start pb-24 px-4 max-sm:px-0">
+      <CodeEditor />
+      <Dock />
     </main>
   );
 }
