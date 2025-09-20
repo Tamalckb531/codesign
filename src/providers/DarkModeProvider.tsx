ba575f8758d7c475107815dfa6927883795149fb
Tamal <ckbtamaldipnew@gmail.com>
2025-09-20 22:05:47 +0600

    add Dark Mode Provider

---
+++ src/providers/DarkModeProvider.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { DarkModeContext } from "@/context/darkModeContext";
+import { ReactNode, useEffect, useState } from "react";
+
+interface DarkModeProviderProps {
+  children: ReactNode;
+}
+
+const DarkModeProvider: React.FC<DarkModeProviderProps> = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean | null>(false);
+
+  useEffect(() => {
+    const storedPreference = localStorage.getItem("theme");
+    const prefersDarkMode = storedPreference === "dark";
+
+    setIsDarkMode(prefersDarkMode);
+
+    if (prefersDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    document.documentElement.style.overflowY = "auto";
+  }, []);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => {
+      const newValue = !prev;
+      localStorage.setItem("theme", newValue ? "dark" : "light");
+      return newValue;
+    });
+  };
+
+  if (!isDarkMode) return null;
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export default DarkModeProvider;
